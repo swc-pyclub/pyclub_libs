44d6888877414a5dff36b3bda5d3d00df8e5a0c7
Charly Rousseau <c.rousseau@ucl.ac.uk>
2017-11-15 17:59:11 +0000

    Fixes test_print_utils (and renames to consistent name for repo

---
+++ test_print_utils.py
@@ -0,0 +1,18 @@
+import pytest
+
+import print_utils
+
+
+@pytest.mark.parametrize("test_input, test_color, bold, expected_output", [
+    ('a', 'red', 1, '\x1b[31;1ma\x1b[0m'),
+    ('a', 'blue', 1, '\x1b[34;1ma\x1b[0m'),
+    ('a', 'blue', 0, '\x1b[34ma\x1b[0m')
+])
+def test_shell_format(test_input, test_color, bold, expected_output):
+    assert print_utils.shell_hilite(test_input, test_color, bold) == expected_output
+
+
+def test_shell_format_raises():
+    with pytest.raises(AttributeError):
+        print_utils.shell_hilite("a", "not_a_color")
+
