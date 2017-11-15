ffcfea17071e7a6f667f43f18ef3ed63f67cb48e
Charly Rousseau <c.rousseau@ucl.ac.uk>
2017-11-15 18:21:00 +0000

    Fixes indentation of test_debug_utils.py

--- test_debug_utils.py
+++ test_debug_utils.py
@@ -5,11 +5,12 @@ from debug_utils import log_all_variables
 
 @log_all_variables()
 def do_something():
-	a = 10
-	b = 20
-	b = b*3
-	c = b - a
-	return c 
+    a = 10
+    b = 20
+    b = b*3
+    c = b - a
+    return c
 
 
 do_something()
+
