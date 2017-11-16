6158d6a777e883016dfd4aa42767a56b43ab6a77
Charly Rousseau <c.rousseau@ucl.ac.uk>
2017-11-16 15:35:18 +0000

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+
+requirements = ['numpy']
+
+
+setup(
+    name='pyclub_libs',
+    version='0.1',
+    packages=['pyclub_libs',
+              'pyclub_libs.logging',
+              'pyclub_libs.logging'
+              ],
+    requires=requirements,
+    download_url='git+git://github.com/swc-pyclub/pyclub_libs',
+    url='https://github.com/swc-pyclub/pyclub_libs',
+    license='GPLv3',
+    author='PyClub',
+    author_email='pyclub <pyclub@live.ucl.ac.uk>',
+    description='Libraries from the SWC python club',
+    classifiers=['Intended Audience :: Developers',
+                 'Topic :: Software Development']
+)
+
+
+# TODO: add package_data={} for documentation
+
